0379ad66323ad6924ded30c01907a3cc7fd778da
EkkoLizehan <64797122+EkkoLizehan@users.noreply.github.com>
2021-10-18 19:07:03 -0400

    Update LoginActivity.java

--- app/src/main/java/com/example/seg2105/LoginActivity.java
+++ app/src/main/java/com/example/seg2105/LoginActivity.java
@@ -1,17 +1,20 @@
 package com.example.seg2105;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
+
 public class LoginActivity extends MainActivity{
 
     ImageButton btn1;
     Button btn2, btn3;
+    TextInputEditText input1, input2;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,6 +24,8 @@ public class LoginActivity extends MainActivity{
         btn1 = findViewById(R.id.back_button_l);
         btn3 = findViewById(R.id.login_l);
         btn2 = findViewById(R.id.register_l);
+        input1 = findViewById(R.id.enterUsername_l);
+        input2 = findViewById(R.id.enterPassword_l);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +42,7 @@ public class LoginActivity extends MainActivity{
 
             public void onClick(View view) {
 
-                Intent i=new Intent(getApplicationContext(),RegisterActivity.class);
+                Intent i=new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(i);
 
             }
